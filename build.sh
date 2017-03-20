4da4a61c760e024116da80b2b13fa8cb58942467
jockelmore <je.gaidusch@gmx.de>
2017-03-20 15:13:45 +0100

    Next try to fix this

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 if [ "$1" != "" ]; then
-    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/',params = list(dbname='"$1"'))"
+    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/',encoding='utf-8',params = list(dbname='"$1"'))"
 else
-    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/')"
+    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/',encoding='utf-8')"
 fi
 
 #USAGE: sh build.sh {db-name}
