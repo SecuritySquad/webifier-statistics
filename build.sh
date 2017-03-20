ad296fbf2a0375809fc337598801811a7ea8c849
jockelmore <je.gaidusch@gmx.de>
2017-03-20 14:31:57 +0100

    Updated build script to pass dbname as arg

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/')"
+if [ "$1" != "" ]; then
+    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/',params = list(dbname="$1"))"
+else
+    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/')"
+fi
+
+#USAGE: sh build.sh {db-name}
