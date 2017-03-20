6c6474b743b273a2345bfd1fb9ccc6f4b6110f3e
jockelmore <je.gaidusch@gmx.de>
2017-03-20 14:34:54 +0100

    Updated build script mistake

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 if [ "$1" != "" ]; then
-    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/',params = list(dbname="$1"))"
+    R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/',params = list(dbname='"$1"'))"
 else
     R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/')"
 fi
