5aa53371e1fc2e1233466fc98e604ccea3573119
jockelmore <je.gaidusch@gmx.de>
2017-03-20 13:45:37 +0100

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='"$1"')"
-#USAGE: sh build.sh {Output-dir}
+R -e "rmarkdown::render('webifier-statistics.Rmd',output_file='index.html',output_dir='/var/www/statistics.webifier.de/')"
